dbb136a8deb983f33b6e35c29ce15491d9f11549
atere21 <tosinateret@gmail.com>
2023-04-03 11:45:13 -0600

    data-finance

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
+
+const Navbar = () => {
+    const [nav, setNav] = useState (true);
+
+    const handleNav = () => {
+        setNav(!nav);
+    }
+
+  return (
+
+    <div className='text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
+        <h1 className='w-full text-3xl text-[#00df9a] font-bold '>REACT.</h1>
+        <ul className='md:flex hidden'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>Company</li>
+            <li className='p-4'>Resources</li>
+            <li className='p-4'>About</li>
+            <li className='p-4'>Contact</li>
+        </ul>
+        <div onClick={handleNav} className='block md:hidden cursor-pointer'>
+            {!nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20}/>}
+            
+        </div>
+        <div className={! nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500': 'fixed left-[-100%]'}>
+        <h1 className='w-full text-3xl text-[#00df9a] font-bold '>REACT.</h1>
+            <ul className='p-4  uppercase'>
+            <li className='p-4 border-b border-gray-600 '>Home</li>
+            <li className='p-4 border-b border-gray-600 '>Company</li>
+            <li className='p-4 border-b border-gray-600 '>Resources</li>
+            <li className='p-4 border-b border-gray-600 '>About</li>
+            <li className='p-4 border-b border-gray-600 '>Contact</li>
+            </ul>
+        </div>
+    </div>
+
+  )
+}
+
+export default Navbar;
